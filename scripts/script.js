96cd5c8120ff0f802b1b4707c462f0605e2caf98
Shaqnificent <shaqnificent@gmail.com>
2022-04-19 21:08:38 -0500

    Updated the documents

--- scripts/script.js
+++ scripts/script.js
@@ -1,62 +1,62 @@
 function closeNav() {
     //document.getElementById("mySidenav").style.width = "350px";
-    document.getElementById("nav-container").style.gridTemplateColumns = "0% 100%";
-    document.getElementById("grid-container").style.gridTemplateColumns = "3% auto";
-    var element = document.getElementById("navRouter");
-    element.classList.add("hidden");
+    // document.getElementById("nav-container").style.gridTemplateColumns = "0% 100%";
+    // document.getElementById("grid-container").style.gridTemplateColumns = "3% auto";
+    // var element = document.getElementById("navRouter");
+    // element.classList.add("hidden");
 
-    var element = document.getElementById("imgGrid");
-    element.classList.add("hidden");
+    // var element = document.getElementById("imgGrid");
+    // element.classList.add("hidden");
 
-    var element = document.getElementById("content");
-    element.classList.add("hidden");
+    // var element = document.getElementById("content");
+    // element.classList.add("hidden");
 
-    element = document.getElementById("imgGit").style.display = "none";
-    element = document.getElementById("imgLin").style.display = "none";
-    element = document.getElementById("imgTwit").style.display = "none";
+    // element = document.getElementById("imgGit").style.display = "none";
+    // element = document.getElementById("imgLin").style.display = "none";
+    // element = document.getElementById("imgTwit").style.display = "none";
    
 
-    var close = document.getElementById("closeNav");
-    close.classList.add("hidden");
+    // var close = document.getElementById("closeNav");
+    // close.classList.add("hidden");
 
-    var open = document.getElementById("openNav");
-    open.classList.remove("hidden");
+    // var open = document.getElementById("openNav");
+    // open.classList.remove("hidden");
 
-    document.getElementById("nav").style.backgroundColor = "#04F0FF";
+    // document.getElementById("nav").style.backgroundColor = "#04F0FF";
 
-    element = document.getElementById("titleJob").style.color = "white";
-    element = document.getElementById("titlelName").style.color = "white";
-    element = document.getElementById("titleName").style.color = "#04F0FF";
-    element = document.getElementById("Name").style.color = "#04F0FF";
-    element = document.getElementById("display").style.backgroundColor = "rgba(0, 0, 0, 0.9)";
+    // element = document.getElementById("titleJob").style.color = "white";
+    // element = document.getElementById("titlelName").style.color = "white";
+    // element = document.getElementById("titleName").style.color = "#04F0FF";
+    // element = document.getElementById("Name").style.color = "#04F0FF";
+    // element = document.getElementById("display").style.backgroundColor = "rgba(0, 0, 0, 0.9)";
 
 
   }
   
   function openNav() 
   {
-    document.getElementById("nav-container").style.gridTemplateColumns = "90% auto";
-    document.getElementById("grid-container").style.gridTemplateColumns = "30% auto";
-    var element = document.getElementById("navRouter");
-    element.classList.remove("hidden");
-    var close = document.getElementById("openNav");
-    close.classList.add("hidden");
+    // document.getElementById("nav-container").style.gridTemplateColumns = "90% auto";
+    // document.getElementById("grid-container").style.gridTemplateColumns = "30% auto";
+    // var element = document.getElementById("navRouter");
+    // element.classList.remove("hidden");
+    // var close = document.getElementById("openNav");
+    // close.classList.add("hidden");
 
-    var element = document.getElementById("content");
-    element.classList.remove("hidden");
+    // var element = document.getElementById("content");
+    // element.classList.remove("hidden");
 
-    element = document.getElementById("imgGit").style.display = "inline";
-    element = document.getElementById("imgLin").style.display = "inline";
-    element = document.getElementById("imgTwit").style.display = "inline";
+    // element = document.getElementById("imgGit").style.display = "inline";
+    // element = document.getElementById("imgLin").style.display = "inline";
+    // element = document.getElementById("imgTwit").style.display = "inline";
 
-    element = document.getElementById("titleJob").style.color = "black";
-    element = document.getElementById("titlelName").style.color = "black";
-    element = document.getElementById("titleName").style.color = "blue";
-    element = document.getElementById("Name").style.color = "blue";
-    element = document.getElementById("display").style.backgroundColor = "white";
+    // element = document.getElementById("titleJob").style.color = "black";
+    // element = document.getElementById("titlelName").style.color = "black";
+    // element = document.getElementById("titleName").style.color = "blue";
+    // element = document.getElementById("Name").style.color = "blue";
+    // element = document.getElementById("display").style.backgroundColor = "white";
 
-    var open = document.getElementById("closeNav");
-    open.classList.remove("hidden");
-    document.getElementById("nav").style.backgroundColor = "blue";
-    document.getElementById("grid-container").style.backgroundColor = "";
+    // var open = document.getElementById("closeNav");
+    // open.classList.remove("hidden");
+    // document.getElementById("nav").style.backgroundColor = "blue";
+    // document.getElementById("grid-container").style.backgroundColor = "";
   }  
