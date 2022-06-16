da2fd96df88bbc222bf66b7c719a403d1573b005
Shaqnificent <shaqnificent@gmail.com>
2022-06-16 15:44:35 -0500

    finalized home screen animation

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ $(document).ready(function(){
         $("#nav-grid").fadeTo(10,0.01);
         $("#nav-grid").animate({width: "300px",}, {duration: 500, easing: "linear", 
         complete: function(){ 
-            
+            $("#display").css("background-color", "white");
+            $(".secChange").css("color", "black");
+            $(".priChange").css("color", "blue");
             $("#nav-close").removeClass("hidden");
             $("#nav-open").addClass("hidden");
             $("#nav-grid").fadeTo(10,1);
@@ -20,6 +22,9 @@ $(document).ready(function(){
         $("#nav-grid").fadeTo(10,0.01);
         $("#nav-grid").animate({width: "0px",}, {duration: 500, easing: "linear", 
             complete: function(){
+                $("#display").css("background-color", "black");
+                $(".secChange").css("color", "white");
+                $(".priChange").css("color", "rgb(34, 211, 238)");
                 $("#nav-grid").hide();
                 $("#nav-close").addClass("hidden");
                 $("#nav-open").removeClass("hidden");
